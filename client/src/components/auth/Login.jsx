a34fab1d7d7180163a130aa3800f555c7b769c3e
kifo99 <ivan123rakovic@gmail.com>
2025-12-25 22:32:23 +0100

    feat: redesign login page UI (WIP)

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,7 +1,28 @@
+import './login.css';
+
 export function Login() {
   return (
-    <div>
-      <h1>Welcome to login page</h1>
+    <div className="login">
+      <h1 className="login-title">LOGIN</h1>
+      <form className="login-form" action="">
+        <div className="login-field">
+          <input
+            className="login-input"
+            type="email"
+            name="email"
+            placeholder="Email address"
+          />
+        </div>
+        <div className="login-field">
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+      </form>
+      <button className="login-button">Login</button>
     </div>
   );
 }
