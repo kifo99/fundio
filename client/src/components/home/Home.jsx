62a64cd284a0845a95066040b6ca7818618d73f3
kifo99 <ivan123rakovic@gmail.com>
2026-01-10 20:45:28 +0100

    chore: clean up Home page and remove test content

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,14 +3,5 @@ import { logout } from '../../store/authSlice';
 export function Home() {
   const dispatch = useDispatch();
 
-  function handleLogout() {
-    console.log('Clicked!');
-    dispatch(logout());
-    // dispatch({ type: 'USER_LOGOUT' });
-  }
-  return (
-    <div>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  );
+  return <div></div>;
 }
