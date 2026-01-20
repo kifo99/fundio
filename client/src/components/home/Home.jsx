ed961db1bd8b7041fffc924d04cc2c16867a6c3f
kifo99 <ivan123rakovic@gmail.com>
2026-01-20 19:35:14 +0100

    style: finish hero section styling on Home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,19 +5,30 @@ import { Section } from '../section/Section.jsx';
 import './home.scss';
 export function Home() {
   const dispatch = useDispatch();
-
+  const backTextRef = useRef();
   return (
     <article className="section--wrapper">
       <Section
         variant="hero"
         imgUrl="/img/home-images/tomasz-filipek-joOVC9d-jis-unsplash.jpg"
+        backTextRef={backTextRef}
       >
         <div className="section__title">
-          <h1>Supporting Local Businesses, Together</h1>
+          <h1>Supporting Local Businesses</h1>
+          <div className="back__text" ref={backTextRef}>
+            <span>T</span>
+            <span>O</span>
+            <span>G</span>
+            <span>E</span>
+            <span>T</span>
+            <span>H</span>
+            <span>E</span>
+            <span>R</span>
+          </div>
         </div>
-        <div className="section__body">
+        {/* <div className="section__body">
           <button>JOIN US</button>
-        </div>
+        </div> */}
       </Section>
       <Section>
         <h1>Section 1</h1>
@@ -30,42 +41,4 @@ export function Home() {
       </Section>
     </article>
   );
-}
-{
-  /* <section
-        className="home-scroll-section"
-        style={{
-          backgroundImage:
-            "url('/img/home-images/tomasz-filipek-joOVC9d-jis-unsplash.jpg')",
-        }}
-      >
-        <h1>Section Number 1</h1>
-      </section>
-      <section
-        className="home-scroll-section"
-        style={{
-          backgroundImage:
-            "url('/img/home-images/dan-meyers-IQVFVH0ajag-unsplash.jpg')",
-        }}
-      >
-        <h1>Section Number 2</h1>
-      </section>
-      <section
-        className="home-scroll-section"
-        style={{
-          backgroundImage:
-            "url('/img/home-images/jacopo-maiarelli--gOUx23DNks-unsplash.jpg')",
-        }}
-      >
-        <h1>Section Number 3</h1>
-      </section>
-      <section
-        className="home-scroll-section"
-        style={{
-          backgroundImage:
-            "url('/img/home-images/maximilian-muller-cFYt748c9xY-unsplash.jpg')",
-        }}
-      >
-        <h1>Section Number 4</h1>
-      </section> */
 }
--- client/src/components/section/Section.jsx
+++ client/src/components/section/Section.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
 import './section.scss';
 
-export function Section({ variant = 'default', imgUrl = '', children }) {
+export function Section({
+  variant = 'default',
+  imgUrl = '',
+  backTextRef,
+  children,
+}) {
+  useEffect(() => {
+    if (!backTextRef?.current) {
+      console.log('Reference does not exist');
+      return;
+    }
+
+    const spans = backTextRef.current.querySelectorAll('span');
+
+    spans.forEach((element) => {
+      element.classList.add('active');
+    });
+  }, [backTextRef]);
+
   return (
     <section
       className={`section section--${variant}`}
