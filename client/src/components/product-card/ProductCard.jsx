0e4c5799e7e1b790fb372b40198faefb76e794bd
kifo99 <ivan123rakovic@gmail.com>
2026-02-01 20:40:27 +0100

    style: added visual changes to product card component

--- client/src/components/product-card/ProductCard.jsx
+++ client/src/components/product-card/ProductCard.jsx
@@ -1,4 +1,5 @@
 import './productCard.scss';
+import { ShoppingBasket } from 'lucide-react';
 
 // TODO Add proper animation on this component
 export function ProductCard({ imgUrl, productName, price }) {
@@ -7,11 +8,24 @@ export function ProductCard({ imgUrl, productName, price }) {
       <div className="product-card__img">
         <img src={`${imgUrl}`} alt="Image of product" />
       </div>
-      <div className="product-card__name">
-        <h3>{productName}</h3>
+      <div className="product-card__body">
+        <div className="product-card__header">
+          <h3 className="header__title">{productName}</h3>
+          <p>{price}$</p>
+        </div>
+        <div className="product-card__description">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
+            nostrum nihil magni error qui, quaerat nemo alias vitae veniam,
+            aspernatur tempore numquam, eius consequuntur sit praesentium
+            necessitatibus debitis voluptates. Deleniti.
+          </p>
+        </div>
       </div>
-      <div className="product-card__price">
-        <p>{price}$ kg</p>
+      <div className="product-card__footer">
+        <i>
+          <ShoppingBasket />
+        </i>
       </div>
     </div>
   );
