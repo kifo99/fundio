7cb383da45abc367ae3d732d16f154ee7ad2cdf0
kifo99 <ivan123rakovic@gmail.com>
2026-02-04 15:52:41 +0100

    feat: changed routes for product

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -3,13 +3,13 @@ import express from 'express';
 const route = express.Router();
 
 // TODO create route to retrieve all products that vendor created
-route.get('/:id/products');
+route.get('/vendors/:vendorId/products');
 
 // TODO create route that will create product
-route.post('/product');
+route.post('/vendors/:vendorId/product');
 
 // TODO create route that will remove product
-route.delete('/:id');
+route.delete('/products/:productId');
 
 // TODO create route that will edit product
-route.put('/:id');
+route.put('/products/:productsId');
