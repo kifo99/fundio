191723e4895e791c459e73c60b5b3ace3ca61a9c
kifo99 <ivan123rakovic@gmail.com>
2026-02-04 18:38:30 +0100

    clean: cleaning up routes file

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,17 +1,22 @@
 import express from 'express';
-
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getVendorsProducts,
+} from '../controllers/product.js';
 const route = express.Router();
 
 // TODO create route to retrieve all products that vendor created
-route.get('/vendors/:vendorId/products');
+route.get('/vendors/:vendorId/products', getVendorsProducts);
 
 // TODO create route that will create product
-route.post('/vendors/:vendorId/product');
+route.post('/vendors/:vendorId/product', addProduct);
 
 // TODO create route that will remove product
-route.delete('/products/:productId');
+route.delete('/products/:productId', deleteProduct);
 
 // TODO create route that will edit product
-route.put('/products/:productsId');
+route.put('/products/:productId', editProduct);
 
 export default route;
