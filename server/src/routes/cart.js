b6a810264cd381d77d5a405f94f76b4d5d612321
kifo99 <ivan123rakovic@gmail.com>
2026-02-06 16:50:28 +0100

    fix: added missing route in cart

--- server/src/routes/cart.js
+++ server/src/routes/cart.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import { addCart, deleteCart, getCart } from '../controllers/cart.js';
+import { addCart, deleteCart, getCart, getItems } from '../controllers/cart.js';
 const route = express.Router();
 
-// TODO Create route that will retrieve users cart
+// route that will retrieve users cart
 route.get('/:userId', getCart);
 
-// TODO Create route that will add cart for user
+// rout that will retrieve all items inside the cart
+route.get('/:cartId/items', getItems);
+
+// route that will add cart for user
 route.post('/:userId', addCart);
 
-// TODO Create route that will delete cart
+// route that will delete cart
 route.delete('/:cartId', deleteCart);
 
 export default route;
