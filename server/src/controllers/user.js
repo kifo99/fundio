4ed57593efc7d36ad243b5464dc1e345d35a4465
kifo99 <ivan123rakovic@gmail.com>
2026-02-07 21:03:34 +0100

    refactor: added new error handeling and refactord async code of getUser controller

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,23 +1,17 @@
 import User from '../data/models/User.js';
+import ApiError from '../utils/ApiError.js';
+import catchAsync from '../utils/catchAsync.js';
 
-// TODO create getUser function
+export const getUser = catchAsync(async (req, res, next) => {
+  const userId = req.body.id;
 
-export async function getUser(req, res, next) {
-  try {
-    const userId = req.body.id;
-    console.log(userId);
-    const user = await User.query().findById(userId);
-    console.log(user);
-    if (!user) throw new Error('User not founded!');
+  if (!userId) throw new ApiError('User id not valid', 400);
+  const user = await User.query().findById(userId);
 
-    res.status(200).json({
-      user: user,
-      message: 'User has been found',
-    });
-  } catch (error) {
-    console.error(error.message);
-    res.status(error.statusCode || 500).json({
-      message: `There has been an error: ${error.message}`,
-    });
-  }
-}
+  if (!user) throw new ApiError('User not founded!', 404);
+
+  res.status(200).json({
+    user: user,
+    message: 'User has been found',
+  });
+});
