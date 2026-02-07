9c95efdb81ef93b5fc92fe69d1ba8011a1ec3878
kifo99 <ivan123rakovic@gmail.com>
2026-02-07 20:27:57 +0100

    refactor: added new error handeling and refactord async code of verifyVendor controller

--- server/src/controllers/admin.js
+++ server/src/controllers/admin.js
@@ -1,4 +1,6 @@
 import User from '../data/models/User.js';
+import ApiError from '../utils/ApiError.js';
+import catchAsync from '../utils/catchAsync.js';
 
 export async function getVendor(req, res, next) {
   try {
@@ -21,43 +23,36 @@ export async function getVendor(req, res, next) {
   }
 }
 
-// TODO Make function work dynamically and update data based on admins input
-export async function verifyVendor(req, res, next) {
-  try {
-    const { id } = req.params;
-    const { isVerified } = req.body;
+export const verifyVendor = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const { isVerified } = req.body;
 
-    if (!id) throw new Error('User id does not exist');
+  if (!id) throw new ApiError('User id not valid', 400);
 
-    const user = await User.query().findById(id);
-    if (!user) throw new Error('User is not found');
+  const user = await User.query().findById(id);
+  if (!user) throw new ApiError('User is not found', 404);
 
-    if (!isVerified) {
-      await user.$query().patch({
-        role: 'user',
-        emailVerified: false,
-        vendorStatus: 'rejected',
-      });
-
-      res.status(200).json({
-        message: 'Vendor verification denied.',
-        user: user,
-      });
-    }
-
+  if (!isVerified) {
     await user.$query().patch({
-      role: 'vendor',
-      emailVerified: true,
-      vendorStatus: 'verified',
+      role: 'user',
+      emailVerified: false,
+      vendorStatus: 'rejected',
     });
 
     res.status(200).json({
-      message: 'Vendor verified',
+      message: 'Vendor verification denied.',
       user: user,
     });
-  } catch (error) {
-    res.status(error.statusCode || 500).json({
-      message: error.message,
-    });
   }
-}
+
+  await user.$query().patch({
+    role: 'vendor',
+    emailVerified: true,
+    vendorStatus: 'verified',
+  });
+
+  res.status(200).json({
+    message: 'Vendor verified',
+    user: user,
+  });
+});
