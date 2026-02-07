1615d4fa48155957c85c191fc8343eebc3ac069d
kifo99 <ivan123rakovic@gmail.com>
2026-02-07 20:54:02 +0100

    refactor: added new error handeling and refactord async code of editProduct controller

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -52,29 +52,23 @@ export const deleteProduct = catchAsync(async (req, res, next) => {
 });
 
 // Controller for product route that edit products
-export const editProduct = async (req, res, next) => {
-  try {
-    const productId = req.params.productId;
-    if (!productId) throw new Error('Product id was not passed');
-    const { productName, price, productImg, description, discount } = req.body;
+export const editProduct = catchAsync(async (req, res, next) => {
+  const productId = req.params.productId;
+  if (!productId) throw new ApiError('Product id was not valid', 400);
+  const { productName, price, productImg, description, discount } = req.body;
 
-    const product = await Product.query().findById(productId);
-    if (!product) throw new Error('Product not found');
-    await product.$query().patch({
-      productName,
-      price,
-      productImg,
-      description,
-      discount,
-    });
+  const product = await Product.query().findById(productId);
+  if (!product) throw new ApiError('Product not found', 404);
+  await product.$query().patch({
+    productName,
+    price,
+    productImg,
+    description,
+    discount,
+  });
 
-    res.status(200).json({
-      message: 'Successfully added product',
-      product: product,
-    });
-  } catch (error) {
-    res.status(error.statusCode || 500).json({
-      message: `Error: ${error.message}`,
-    });
-  }
-};
+  res.status(200).json({
+    message: 'Successfully added product',
+    product: product,
+  });
+});
