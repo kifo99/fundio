0461f93d7e9c7686c886a27aead8d55e62241331
kifo99 <ivan123rakovic@gmail.com>
2026-03-25 12:06:20 +0100

    style: redesigning ProductCard component

--- client/src/queries/auth.queries.js
+++ client/src/queries/auth.queries.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 const login = async function (data) {
   try {
