2fe44dd05c3e90dff66788d8e23ae22b3174d908
kifo99 <ivan123rakovic@gmail.com>
2026-09-21 15:33:17 +0200

    fix

--- client/src/pages/auth/signup/Auth.jsx
+++ client/src/pages/auth/signup/Auth.jsx
@@ -4,7 +4,7 @@ export function Auth() {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center h-dvh w-full sm:w-2/3 md:w-1/2 lg:w-1/2 m-auto">
       <div className="relative h-full w-full flex items-center justify-center overflow-hidden">
-        <div className="relative w-full h-full sm:w-2/3 md:w-1/2 lg:w-1/3 overflow-hidden">
+        <div className="relative w-full h-full lg:w-1/2 overflow-hidden">
           <AuthCompact />
         </div>
       </div>
